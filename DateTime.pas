f8a3bb40b33bfa2d72b9729cdf66abbd6833c9be
Shijeroo <vladermilov4@gmail.com>
2024-10-05 01:42:30 +0300

    Add is leap year

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,13 @@
 begin
-
+//Високостность года
+  var Year := ReadInteger('Введите год :');
+  Assert(Year >= 0, 'Год не может быть отрицательным!');
+  var flag := False;
+  
+  if Year.Divs(4) then
+    if Year.Divs(100) and not Year.Divs(400) then
+      flag := False
+    else
+      flag := True;
+  Print($'Год високостный :{flag}');
 end.
